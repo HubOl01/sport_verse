87f2f40529935f66354e45e7720f2f405f751212
Oleg Kulichkov <kuloleg52@gmail.com>
2023-11-13 23:25:21 +0300

    added folder mobile_JC

---
+++ mobile_JC/app/src/main/java/com/example/sportsphere/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.sportsphere
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.sportsphere.ui.theme.SportSphereTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SportSphereTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainPage()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun MainPage() {
+    Scaffold(
+//        topBar = {
+//            TopAppBar(
+//                colors = TopAppBarDefaults.mediumTopAppBarColors(
+//                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                    titleContentColor = MaterialTheme.colorScheme.primary,
+//                ),
+//                title = {
+//                    Text("Top app bar")
+//                }
+//            )
+//        }
+    ) {it ->  Column(
+        Modifier.fillMaxWidth()
+    ) {
+        Stories(it = it)
+    }
+
+
+    }
+
+}
+
+@Composable
+fun Stories(it: PaddingValues){
+    LazyRow(
+
+        Modifier
+            .padding(it)
+            .fillMaxWidth(),
+    ) {
+        items(6){
+            Card(
+                modifier = Modifier
+                    .height(150.dp)
+                    .width(100.dp)
+                    .padding(top = 10.dp, bottom = 10.dp, start = 10.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.Red
+                )
+            ) {
+
+            }
+        }
+    }
+}
