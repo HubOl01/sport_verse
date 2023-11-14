ae8197360ff9192bc8db2e4ab54b4f7d13ce5305
Oleg Kulichkov <kuloleg52@gmail.com>
2023-11-14 18:22:19 +0300

    added code

--- mobile_JC/app/src/main/java/com/example/sportsphere/MainActivity.kt
+++ mobile_JC/app/src/main/java/com/example/sportsphere/MainActivity.kt
@@ -3,19 +3,28 @@ package com.example.sportsphere
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -25,8 +34,12 @@ import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.sportsphere.ui.theme.SportSphereTheme
@@ -49,7 +62,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun MainPage() {
     Scaffold(
@@ -64,11 +77,12 @@ fun MainPage() {
 //                }
 //            )
 //        }
-    ) {it ->  Column(
-        Modifier.fillMaxWidth()
-    ) {
-        Stories(it = it)
-    }
+    ) { it ->
+        Column(
+            Modifier.fillMaxWidth()
+        ) {
+            Stories(it = it)
+        }
 
 
     }
@@ -76,14 +90,14 @@ fun MainPage() {
 }
 
 @Composable
-fun Stories(it: PaddingValues){
+fun Stories(it: PaddingValues) {
     LazyRow(
 
         Modifier
             .padding(it)
             .fillMaxWidth(),
     ) {
-        items(6){
+        items(6) {
             Card(
                 modifier = Modifier
                     .height(150.dp)
@@ -94,6 +108,61 @@ fun Stories(it: PaddingValues){
                 )
             ) {
 
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Post() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                modifier = Modifier.padding(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(32.dp)
+                        .clip(CircleShape)
+                        .background(color = Color.Blue)
+                )
+                Box(modifier = Modifier.width(10.dp))
+                Text(text = "User1")
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(4f / 4f)
+                    .background(color = Color.Red)
+            ) {
+
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp, vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row() {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_favorite_border_24),
+                        contentDescription = "like"
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_favorite_border_24),
+                        contentDescription = "like"
+                    )
+                }
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_favorite_border_24),
+                    contentDescription = "like"
+                )
             }
         }
     }
