f6551ae4b5b015733f15de667ef8fe278b731fb1
Oleg Kulichkov <kuloleg52@gmail.com>
2025-05-05 21:58:34 +0300

    fix code on vercel

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,14 +1,33 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "tailwindcss";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   server: {
+//     host: "0.0.0.0",
+//     port: 5173,
+//   },
+
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ["date-fns", "@mui/x-date-pickers"],
+//   },
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss()],
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
   },
-
   plugins: [react()],
   optimizeDeps: {
     include: ["date-fns", "@mui/x-date-pickers"],
@@ -17,5 +36,8 @@ export default defineConfig({
     postcss: {
       plugins: [tailwindcss()],
     },
+  },
+  build: {
+    outDir: "dist",
   },
 });
